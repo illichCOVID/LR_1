e2776405afe930956616399fa121684c27fc9e27
ilyha <84225811+illichCOVID@users.noreply.github.com>
2023-01-02 17:25:50 +0300

    Add files via upload

---
+++ lb6(7)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        // Метод для реверсу масиву
+        static int[] ReverseArray(int[] array)
+        {
+            int[] reverse = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                reverse[i] = array[array.Length - i - 1];
+            }
+            return reverse;
+        }
+
+        static void Main(string[] args)
+        {
+            // Створюємо тестовий масив
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            // Виводимо масив у зворотному порядку
+            Console.WriteLine("Масив у зворотному порядку: " + string.Join(", ", ReverseArray(array)));
+        }
+    }
+}
