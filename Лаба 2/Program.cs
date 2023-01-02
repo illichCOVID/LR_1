6cd86e018db3ee9df24270eb1b4cae15a7f9d03b
ilyha <84225811+illichCOVID@users.noreply.github.com>
2023-01-02 17:27:05 +0300

    Add files via upload

---
+++ Лаба 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int k = Convert.ToInt32(Console.ReadLine());
+        if (n * n > n * k)
+        {
+            return;
+        }
+        double sum1 = 0;
+        double sum2 = 0;
+        for (int i = n * n; i < n * k; ++i)
+        {
+            sum1 += (i * i - 3) / (Math.Pow(-1, i) * i * i + 5);
+            sum2 += (i * i + Math.Pow(-1, i) * i - 1) / (i * i + 1);
+        }
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+    }
+}
