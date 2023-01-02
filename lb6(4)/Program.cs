82afc2b3d8422fd4218c0d67ec20ce1819dc56c3
ilyha <84225811+illichCOVID@users.noreply.github.com>
2023-01-02 17:24:39 +0300

    Add files via upload

---
+++ lb6(4)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReverseStringWithDelimiter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Зчитуємо строку з клавіатури
+            Console.Write("Введіть строку: ");
+            string input = Console.ReadLine();
+
+            // Знаходимо перший вхідження "магічного знаку" у строку
+            int delimiterIndex = input.IndexOf(',');
+
+            // Виводимо частину строки перед "магічним знаком" у зворотному порядку
+            Console.Write("Строка у зворотному порядку: ");
+            for (int i = delimiterIndex - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+
+            // Виводимо "магічний знак"
+            Console.Write(",");
+
+            // Виводимо частину строки після "магічного знаку" у зворотному порядку
+            for (int i = input.Length - 1; i > delimiterIndex; i--)
+            {
+                Console.Write(input[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
